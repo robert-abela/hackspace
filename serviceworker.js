7b1f11925e330eb6b0d342dc0c8d92ed949f606a
Robert Abela <robert.abela@mcast.edu.mt>
2021-09-27 22:19:33 +0200

    attempt at offline pwa

--- serviceworker.js
+++ serviceworker.js
@@ -1,19 +1,40 @@
 var staticCacheName = "pwa";
 
-self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"]);
-	})
-);
-});
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+	  caches.open(cacheName).then(function(cache) {
+		return cache.addAll(
+		  [
+			'/404.html',
+			'/article-logo-launch.html',
+			'/article-start-journey.html',
+			'/articles-list.html',
+			'/index.html',
+			'/join.html',
+			'/planned-sessions.html',
+			'/assets/favicon.ico',
+			'/assets/icon-192x192.png',
+			'/assets/icon-512x512.png',
+			'/assets/logo-horizontal-big.png',
+			'/assets/logo-horizontal-med.png',
+			'/assets/logo-vertical-big.png',
+			'/assets/photos/female-865110_1280.jpg',
+			'/assets/photos/path-6567149_1280.jpg',
+			'/css/404.css',
+			'/css/styles.css',
+			'/js/egg.min.js',
+			'/js/scripts.js'
+		  ]
+		);
+	  })
+	);
+  });
+  
 
 self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
-
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
 });
