0140f0c659c2f5ac0b9599eb86ebc79e498f7378
Robert Abela <robert.abela@mcast.edu.mt>
2021-09-27 22:36:17 +0200

    attempt at fix for caching

--- serviceworker.js
+++ serviceworker.js
@@ -1,33 +1,31 @@
-var staticCacheName = "pwa";
+const cacheName = "HackSpacePWAv1";
+const appShellFiles = [
+	'/404.html',
+	'/article-logo-launch.html',
+	'/article-start-journey.html',
+	'/articles-list.html',
+	'/index.html',
+	'/join.html',
+	'/planned-sessions.html',
+	'/assets/favicon.ico',
+	'/assets/icon-192x192.png',
+	'/assets/icon-512x512.png',
+	'/assets/logo-horizontal-big.png',
+	'/assets/logo-horizontal-med.png',
+	'/assets/logo-vertical-big.png',
+	'/assets/photos/female-865110_1280.jpg',
+	'/assets/photos/path-6567149_1280.jpg',
+	'/css/404.css',
+	'/css/styles.css',
+	'/js/egg.min.js',
+	'/js/scripts.js'
+];
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			return cache.addAll(
-			[
-				'/404.html',
-				'/article-logo-launch.html',
-				'/article-start-journey.html',
-				'/articles-list.html',
-				'/index.html',
-				'/join.html',
-				'/planned-sessions.html',
-				'/assets/favicon.ico',
-				'/assets/icon-192x192.png',
-				'/assets/icon-512x512.png',
-				'/assets/logo-horizontal-big.png',
-				'/assets/logo-horizontal-med.png',
-				'/assets/logo-vertical-big.png',
-				'/assets/photos/female-865110_1280.jpg',
-				'/assets/photos/path-6567149_1280.jpg',
-				'/css/404.css',
-				'/css/styles.css',
-				'/js/egg.min.js',
-				'/js/scripts.js'
-			]
-			);
-		})
-	);
+self.addEventListener('install', (e) => {
+	e.waitUntil((async () => {
+		const cache = await caches.open(cacheName);
+		await cache.addAll(appShellFiles);
+	})());
 });
 
 self.addEventListener("fetch", function (event) {
