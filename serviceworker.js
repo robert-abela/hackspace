b30bc3ab14c0f4a1e553df3a7e3270953aff42ba
Robert Abela <robert@lobeslab.com>
2021-09-28 09:53:42 +0200

    service worker rewrite, test for js req

--- serviceworker.js
+++ serviceworker.js
@@ -1,39 +1,41 @@
-const cacheName = "HackSpacePWAv1";
+const cacheName = 'HackSpacePWAv1'
 const appShellFiles = [
-	'/404.html',
-	'/article-logo-launch.html',
-	'/article-start-journey.html',
-	'/articles-list.html',
-	'/index.html',
-	'/join.html',
-	'/planned-sessions.html',
-	'/assets/favicon.ico',
-	'/assets/icon-192x192.png',
-	'/assets/icon-512x512.png',
-	'/assets/logo-horizontal-big.png',
-	'/assets/logo-horizontal-med.png',
-	'/assets/logo-vertical-big.png',
-	'/assets/photos/female-865110_1280.jpg',
-	'/assets/photos/path-6567149_1280.jpg',
-	'/css/404.css',
-	'/css/styles.css',
-	'/css/bootstrap.min.css',
-	'/js/egg.min.js',
-	'/js/bootstrap.bundle.min.js',
-	'/js/scripts.js'
-];
+  '/404.html',
+  '/article-logo-launch.html',
+  '/article-start-journey.html',
+  '/articles-list.html',
+  '/index.html',
+  '/join.html',
+  '/planned-sessions.html',
+  '/assets/favicon.ico',
+  '/assets/icon-192x192.png',
+  '/assets/icon-512x512.png',
+  '/assets/logo-horizontal-big.png',
+  '/assets/logo-horizontal-med.png',
+  '/assets/logo-vertical-big.png',
+  '/assets/photos/female-865110_1280.jpg',
+  '/assets/photos/path-6567149_1280.jpg',
+  '/css/404.css',
+  '/css/styles.css',
+  '/css/bootstrap.min.css',
+  '/js/egg.min.js',
+  '/js/bootstrap.bundle.min.js',
+  '/js/scripts.js'
+]
 
-self.addEventListener('install', (e) => {
-	e.waitUntil((async () => {
-		const cache = await caches.open(cacheName);
-		await cache.addAll(appShellFiles);
-	})());
-});
+self.addEventListener('install', function (event) {
+  event.waitUntil(cacheAllAssets())
+})
 
-self.addEventListener("fetch", function (event) {
-	event.respondWith(
-		caches.match(event.request).then(function (response) {
-			return response || fetch(event.request);
-		})
-	);
-});
+async function cacheAllAssets() {
+  const cache = await caches.open(cacheName)
+  await cache.addAll(appShellFiles)
+}
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    })
+  )
+})
